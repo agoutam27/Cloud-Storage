e74d2c02beb074b09ea155c7662d5e49e20849f0
akhileshgoutam <akhilesh.goutam@hdworks.in>
2019-05-10 16:34:58 +0530

    readme updated and test cases added

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import routes from './routes.js';
 
 const initRoutes = (app) => {
+  // Set up authentication middleware here, but there might be some apis not requiring authentication
   app.use(`/v1`, routes);
 };
 
--- test/test.js
+++ test/test.js
@@ -2,3 +2,4 @@
 require('babel-register');
 
 // Initiate testing 
+require('./stotageController.test');
